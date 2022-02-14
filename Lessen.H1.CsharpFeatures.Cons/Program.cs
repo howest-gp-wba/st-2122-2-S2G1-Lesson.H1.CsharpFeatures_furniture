b507287d74c77311aaa36315f98ec91a1cc82760
William Schokkelé <william.schokkele@howest.be>
2022-02-14 14:31:31 +0100

    Add project files.

---
+++ Lessen.H1.CsharpFeatures.Cons/Program.cs
@@ -0,0 +1,22 @@
+﻿using Lesson.H1.CsharpFeatures.Domain;
+using System;
+using System.Drawing;
+
+namespace Lessen.H1.CsharpFeatures.Cons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Furniture myChair = new Furniture
+            {
+                Id = 1,
+                Type = FurnitureType.Chair,
+                Description = "Een gemakkelijke stoel",
+                Color = KnownColor.SaddleBrown
+            };
+
+            Console.WriteLine($"Meubelstuk nr. {myChair.Id}: {myChair.Type} - {myChair.Color} \t\t{myChair.Description}");
+        }
+    }
+}
---
+++ Lesson.H1.CsharpFeatures.Domain/Furniture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+
+namespace Lesson.H1.CsharpFeatures.Domain
+{
+    public class Furniture
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public FurnitureType Type { get; set; }
+        public KnownColor Color {get; set;}
+
+    }
+
+    public enum FurnitureType { Chair, Table, Closet, Seat }
+}
