e9d21178c51b3887e768c534a8297ee4a5720bfc
dimitri casier <dimitri.casier@howest.be>
2022-02-17 11:38:38 +0100

    add furniture index and detail functionality

---
+++ Lesson.H1.CsharpFeatures.WebApp/Controllers/FurnitureController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lesson.H1.CsharpFeatures.Domain;
+using Lesson.H1.CsharpFeatures.WebApp.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Lesson.H1.CsharpFeatures.WebApp.Controllers
+{
+    public class FurnitureController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "List of furniture";
+
+            List<Furniture> furnitures = Furniture.GetAll();
+
+            return View(furnitures);
+        }
+
+        public IActionResult Detail()
+        {
+            ViewData["Title"] = "Detail of furniture";
+
+            List<Furniture> furnitures = Furniture.GetAll();
+
+            FurnitureListViewModel furnitureViewModel = new FurnitureListViewModel
+            {
+                Furnitures = furnitures,
+                Count = furnitures.Count,
+                QuoteOfTheDay = "The best furniture in Town"
+            };
+
+            return View(furnitureViewModel);
+        }
+
+
+    }
+}
---
+++ Lesson.H1.CsharpFeatures.WebApp/ViewModels/FurnitureListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Lesson.H1.CsharpFeatures.Domain;
+using System.Collections.Generic;
+
+namespace Lesson.H1.CsharpFeatures.WebApp.ViewModels
+{
+    public class FurnitureListViewModel
+    {
+        public List<Furniture> Furnitures { get; set; }
+
+        public int Count { get; set; }
+
+        public string QuoteOfTheDay { get; set; }
+    }
+}
