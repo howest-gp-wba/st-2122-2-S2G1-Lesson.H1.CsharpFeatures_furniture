58b9476a8e3a9db5af5b0fa3b8290cc4f23504af
William Schokkelé <william.schokkele@howest.be>
2022-02-21 15:56:41 +0100

    Add About Controller

---
+++ Lesson.H1.CsharpFeatures.WebApp/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson.H1.CsharpFeatures.WebApp.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search(string searchkey)
+        {
+            return View(); //todo: create a view named “Search” under Views/Home }
+        }
+    }
+}
